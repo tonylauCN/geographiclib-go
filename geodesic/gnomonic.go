46f3a28ad614d647da663ee7f3e0cd15a92c9cf9
tonylauCN <ryuu.pro@gmail.com>
2023-08-22 13:08:50 +0800

    Update gnomonic.go

--- geodesic/gnomonic.go
+++ geodesic/gnomonic.go
@@ -10,24 +10,44 @@ const NumIt int = 10
 
 var Eps = 0.01 * math.Sqrt(epsilon)
 
-type Gnomonic struct {
-	_earth *Geodesic
-	_a     float64
-	_f     float64
+type GnomonicData struct {
+	Lat0 float64 // latitude of center point of projection (degrees).
+	Lon0 float64 // longitude of center point of projection (degrees).
+	Lat  float64 // latitude of point (degrees).
+	Lon  float64 // longitude of point (degrees).
+	X    float64 // easting of point (meters).
+	Y    float64 // northing of point (meters).
+	Azi  float64 // azimuth of geodesic at point (degrees).
+	Rk   float64 // reciprocal of azimuthal scale at point.
 }
 
-func NewGnomonic(earth *Geodesic) *Gnomonic {
+type Gnomonic struct {
+	earth *Geodesic
+	a     float64
+	f     float64
+}
+
+func NewGnomonic(geo *Geodesic) *Gnomonic {
 	g := &Gnomonic{
-		_earth: earth,
-		_a:     earth.EquatorialRadius(),
-		_f:     earth.Flattening(),
+		earth: geo,
+		a:     geo.EquatorialRadius(),
+		f:     geo.Flattening(),
 	}
 	return g
 }
 
+func EarthGnomonic() *Gnomonic {
+
+	return &Gnomonic{
+		earth: WGS84,
+		a:     WGS84_a,
+		f:     WGS84_f,
+	}
+}
+
 // Forward projection, from geographic to gnomonic.
 func (g *Gnomonic) Forward(lat0, lon0, lat, lon float64) GnomonicData {
-	inv := g._earth.InverseWithCapabilities(lat0, lon0, lat, lon,
+	inv := g.earth.InverseWithCapabilities(lat0, lon0, lat, lon,
 		capabilities.Azimuth|capabilities.GeodesicScale|capabilities.ReducedLength)
 	fwd := GnomonicData{lat0, lon0, lat, lon, math.NaN(), math.NaN(), inv.Azi2, inv.M12}
 	if inv.M12 > 0.0 {
@@ -45,13 +65,13 @@ func (g *Gnomonic) Reverse(lat0, lon0, x, y float64) GnomonicData {
 	rev := GnomonicData{lat0, lon0, math.NaN(), math.NaN(), x, y, math.NaN(), math.NaN()}
 	azi0 := atan2d(x, y)
 	rho := math.Hypot(x, y)
-	s := g._a * math.Atan(rho/g._a)
-	little := rho <= g._a
+	s := g.a * math.Atan(rho/g.a)
+	little := rho <= g.a
 	if !little {
 		rho = 1.0 / rho
 	}
 
-	line := g._earth.LineWithCapabilities(lat0, lon0, azi0, capabilities.Latitude|capabilities.Longitude|capabilities.Azimuth|
+	line := g.earth.LineWithCapabilities(lat0, lon0, azi0, capabilities.Latitude|capabilities.Longitude|capabilities.Azimuth|
 		capabilities.DistanceIn|capabilities.ReducedLength|capabilities.GeodesicScale)
 	count := NumIt
 	trip := 0
@@ -71,7 +91,7 @@ func (g *Gnomonic) Reverse(lat0, lon0, x, y float64) GnomonicData {
 			ds = (rho - pos.M12/pos.M12Reduced) * pos.M12Reduced * pos.M12Reduced
 		}
 		s -= ds
-		if math.Abs(ds) <= Eps*g._a {
+		if math.Abs(ds) <= Eps*g.a {
 			trip++
 		}
 	}
@@ -85,15 +105,4 @@ func (g *Gnomonic) Reverse(lat0, lon0, x, y float64) GnomonicData {
 		rev.Rk = pos.M12
 		return rev
 	}
-}
-
-type GnomonicData struct {
-	Lat0 float64 // latitude of center point of projection (degrees).
-	Lon0 float64 // longitude of center point of projection (degrees).
-	Lat  float64 // latitude of point (degrees).
-	Lon  float64 // longitude of point (degrees).
-	X    float64 // easting of point (meters).
-	Y    float64 // northing of point (meters).
-	Azi  float64 // azimuth of geodesic at point (degrees).
-	Rk   float64 // reciprocal of azimuthal scale at point.
 }
